87e897e2a956d4cb6e797768849ab70c2d6a8ceb
yaimapp <gmonday730-mbp@yahoo.co.jp>
2016-06-03 15:24:38 +0900

    [add]japanese language file added

---
+++ qa-ask-question-vote-lang-ja.php
@@ -0,0 +1,28 @@
+<?php
+
+/*
+	Plugin Name: Ask Question Vote
+	Plugin URI:
+	Plugin Description: Adds Vote buttons to questions
+	Plugin Version: 0.1
+	Plugin Date: 2016-06-02
+	Plugin Author: 38qa.net
+	Plugin Author URI:
+	Plugin License: GPLv2
+	Plugin Minimum Question2Answer Version: 1.7
+	Plugin Update Check URI:
+*/
+
+	return array(
+		// default
+		'caption_before' => '投票前キャプション',
+		'caption_after' => '投票後キャプション',
+		'vote' => '支持する',
+		'unvote' => '支持を取り消す',
+		'custom_css' => 'カスタム CSS',
+	);
+
+
+/*
+	Omit PHP closing tag to help avoid accidental output
+*/
